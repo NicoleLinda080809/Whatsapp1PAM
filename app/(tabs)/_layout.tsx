06a5b5397ea8e44dea66bbc027e651d3442bfbd3
NicoleLinda080809 <nickthundergalo@gmail.com>
2026-06-13 21:11:05 -0300

    Fazendo a tela Index

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,72 @@
-import { Tabs } from "expo-router"
+import { Tabs } from "expo-router";
+import { StatusBar, Text } from "react-native";
+import { AntDesign, Ionicons} from '@expo/vector-icons'; 
+import { View } from "react-native"; 
 
-export default function TabsLayout(){
-    return(
-        <Tabs>
-            <Tabs.Screen/>
-        </Tabs>
-    )
+export default function TabsLayout() {
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#075E54" />
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: "#075E54",
+          tabBarInactiveTintColor: "#999",
+          tabBarStyle: {
+            backgroundColor: "#052b26",
+            height: 60,
+            paddingBottom: 8,
+            paddingTop: 8,
+            borderTopWidth: 0,  
+            elevation: 0,       
+          },
+          headerStyle: {
+            backgroundColor: "#075E54",
+            shadowOpacity: 0,    
+            elevation: 0,        
+            borderBottomWidth: 0, 
+          },
+          headerTintColor: "#FFF",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+          headerShadowVisible: false, 
+        }}
+      >
+        <Tabs.Screen
+  name="index"
+  options={{
+    title: "Whatsapp",
+    headerRight: () => (
+      <View style={{ flexDirection: "row", gap: 15, marginRight: 15 }}>
+        <AntDesign name="camera" size={24} color="white" />
+        <Ionicons name="ellipsis-vertical" size={24} color="white" />
+      </View>
+    ),
+    tabBarIcon: ({ color }) => <Text style={{ fontSize: 24, color }}>💬</Text>,
+  }}
+/>
+        <Tabs.Screen
+          name="ligacoes"
+          options={{
+            title: "Ligações",
+            tabBarIcon: ({ color }) => <Text style={{ fontSize: 24, color }}>📞</Text>,
+          }}
+        />
+        <Tabs.Screen
+          name="atualizacoes"
+          options={{
+            title: "Atualizações",
+            tabBarIcon: ({ color }) => <Text style={{ fontSize: 24, color }}>🔄</Text>,
+          }}
+        />
+        <Tabs.Screen
+          name="comunidade"
+          options={{
+            title: "Comunidade",
+            tabBarIcon: ({ color }) => <Text style={{ fontSize: 24, color }}>👥</Text>,
+          }}
+        />
+      </Tabs>
+    </>
+  );
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,97 @@
-import { View, Text } from "react-native"
+import { View, Text, FlatList, TouchableOpacity, StyleSheet, SafeAreaView } from "react-native";
 
-export default function Index(){
-    return(
-        <View>
-            <Text>Oi</Text>
-        </View>
-    )
+const conversas = [
+  { id: "1", nome: "Mãe", msg: "Tá jantando?", hora: "19:30", letra: "M" },
+  { id: "2", nome: "Tia", msg: "Oi", hora: "19:20", letra: "T" },
+  { id: "3", nome: "2DSM", msg: "Vou trancar o curso", hora: "15:00", letra: "2" },
+  { id: "4", nome: "Pai", msg: "Eita", hora: "12:30", letra: "P" },
+  { id: "5", nome: "Alexandre Cabeludo", msg: "Cade o trabalho?", hora: "11:45", letra: "A" },
+  { id: "6", nome: "Irmã", msg: "ok!", hora: "08:20", letra: "I" },
+];
+
+export default function Conversas() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={conversas}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <TouchableOpacity style={styles.conversaItem}>
+
+            <View style={styles.avatarPlaceholder}>
+              <Text style={styles.avatarLetra}>{item.letra}</Text>
+            </View>
+
+            <View style={styles.infoContainer}>
+              <View style={styles.headerRow}>
+                <Text style={styles.nome}>{item.nome}</Text>
+                <Text style={styles.hora}>{item.hora}</Text>
+              </View>
+              
+
+              <Text style={styles.mensagem}>{item.msg}</Text>
+            </View>
+          </TouchableOpacity>
+        )}
+      />
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#052b26",
+  },
+ 
+  
+  conversaItem: {
+    flexDirection: "row",
+    padding: 15,
+    
+    
+  },
+  avatarPlaceholder: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: "#075E54",
+    marginRight: 15,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  infoNome: {          
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#",
+    marginBottom: 5,
+  },
+  avatarLetra: {
+    color: "#FFF",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  infoContainer: {
+    flex: 1,
+  
+  },
+  headerRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 5,
+  },
+  nome: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#FFF",
+  },
+  hora: {
+    fontSize: 12,
+    color: "#999",
+  },
+  mensagem: {
+    fontSize: 14,
+    color: "#a8a6a6",
+  },
+});
